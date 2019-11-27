b2342247b89c570f459857af6f5698f84fa26e9a
JIO JU <30791788+nahum27@users.noreply.github.com>
2019-11-27 13:20:09 +0900

    import pickle

--- main.py
+++ main.py
@@ -23,57 +23,59 @@ try:
     with open("data.pickle", "rb") as f:
         words, labels, training, output = pickle.load(f)
 
-
-words = []
-labels = []
-docs = []
-docs_x = []
-docs_y = []
-
-
-for intent in data['intents']:
-    for pattern in intent['patterns']:
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(intent['tag'])
-    if intent["tag"] not in labels:
-        labels.append(intent['tag'])
+except:
+    words = []
+    labels = []
+    docs = []
+    docs_x = []
+    docs_y = []
 
 
-
-words = [stemmer.stem(w.lower()) for w in words if w !="?" ]
-words = sorted(list(set(words)))
-
-labels = sorted(labels)
-
-
-training = []
-output = []
-
-out_empty = [0 for _ in range(len(labels))]
+    for intent in data['intents']:
+        for pattern in intent['patterns']:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent['tag'])
+        if intent["tag"] not in labels:
+            labels.append(intent['tag'])
 
 
-for x, doc in enumerate(docs_x):
-    bag = []
 
-    wrds = [stemmer.stem(w) for w in doc]
+    words = [stemmer.stem(w.lower()) for w in words if w !="?" ]
+    words = sorted(list(set(words)))
 
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
+    labels = sorted(labels)
 
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
 
-    training.append(bag)
-    output.append(output_row)
+    training = []
+    output = []
 
-training = numpy.array(training)
-output = numpy.array(output)
+    out_empty = [0 for _ in range(len(labels))]
 
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+            output_row = out_empty[:]
+            output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
 
 
 tensorflow.reset_defaault_graph()
@@ -85,8 +87,12 @@ net = tflearn.fully_connected(net, len(output[0]), activation='softmax')
 
 model = tflearn.DNN(net)
 
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-model.save('model.tflearn')
+
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save('model.tflearn')
 
 
 
